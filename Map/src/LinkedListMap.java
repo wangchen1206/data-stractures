db99dd262b0c984d18b321b0fe335dc51e4ea4ec
chen-wang1 <chen.wang1@hp.com>
2019-12-18 23:00:55 +0800

    基于链表实现的Map 基于二分搜索树实现的Map

---
+++ Map/src/LinkedListMap.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+
+/**
+ * @Author: ck
+ * @Date: 2019/12/18 19:59
+ */
+public class LinkedListMap<K, V> implements Map<K, V> {
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(K key) {
+            this(key, null, null);
+        }
+
+        public Node() {
+            this(null, null, null);
+        }
+
+        @Override
+        public String toString() {
+            return this.key.toString() + " : " + this.value.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedListMap() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    private Node getNode(K key) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.key.equals(key))
+                return cur;
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        Node node = getNode(key);
+        if (node == null) {
+            //添加在链表头部
+            dummyHead.next = new Node(key, value, dummyHead.next);
+            size++;
+        } else {
+            node.value = value;
+        }
+    }
+
+    @Override
+    public V remove(K key) {
+        Node prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.key.equals(key))
+                break;
+            prev = prev.next;
+        }
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size--;
+            return delNode.value;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(key);
+        if (node == null)
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        node.value = newValue;
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
+            for (String word : words) {
+                if (linkedListMap.contains(word))
+                    linkedListMap.set(word,linkedListMap.get(word)+1);
+                else
+                    linkedListMap.add(word,1);
+            }
+            System.out.println("Total different words: "+linkedListMap.getSize());
+            System.out.println("Frequency of pride: "+linkedListMap.get("pride"));
+            System.out.println("Frequency of prejudice: "+linkedListMap.get("prejudice"));
+        }
+    }
+}
--- Map/src/Main.java
+++ Map/src/Main.java
@@ -1,6 +1,36 @@
+import java.util.ArrayList;
+
 public class Main {
 
+    public static double testMap(Map<String,Integer> map,String filename){
+        long startTime = System.nanoTime();
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words) {
+                if (map.contains(word))
+                    map.set(word,map.get(word)+1);
+                else
+                    map.add(word,1);
+            }
+            System.out.println("Total different words: "+map.getSize());
+            System.out.println("Frequency of pride: "+map.get("pride"));
+            System.out.println("Frequency of prejudice: "+map.get("prejudice"));
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime)/1000000000.0;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        String filename = "pride-and-prejudice.txt";
+        BSTMap<String,Integer> bstMap = new BSTMap<>();
+        double time1 = testMap(bstMap,filename);
+        System.out.println("BST Map : "+time1+" s");
+
+        LinkedListMap<String,Integer> linkedListMap = new LinkedListMap<>();
+        double time2 = testMap(linkedListMap,filename);
+        System.out.println("BST Map : "+time2+" s");
+
     }
 }
