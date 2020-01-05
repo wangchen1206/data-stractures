5aeed9159afa72cf4c41f23760a744546f3005e2
chen-wang1 <chen.wang1@hp.com>
2020-01-05 23:08:40 +0800

    init AVLTree

---
+++ AVLTree/src/BSTMap.java
@@ -0,0 +1,192 @@
+import java.util.ArrayList;
+
+/**
+ * @Author: ck
+ * @Date: 2019/12/18 20:51
+ */
+public class BSTMap<K extends Comparable, V> {
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left, right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BSTMap() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    private Node add(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        if (key.compareTo(node.key) < 0)
+            node.left = add(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = add(node.right, key, value);
+        else
+            node.value = value;
+        return node;
+    }
+
+    private Node getNode(Node node, K key) {
+        if (node == null)
+            return null;
+        if (key.compareTo(node.key) == 0)
+            return node;
+        else if (key.compareTo(node.key) > 0)
+            return getNode(node.right, key);
+        else
+            return getNode(node.left, key);
+    }
+
+
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+    public V get(K key) {
+        Node node = getNode(root,key);
+        return node == null?null:node.value;
+    }
+
+    public void set(K key, V newValue) {
+        Node node = getNode(root,key);
+        if (node == null)
+            throw new IllegalArgumentException(key + " doesn't exist!");
+         node.value = newValue;
+    }
+
+    /**
+     * 递归，返回以node为根的二分搜索树的最小值的节点
+     *
+     * @param node
+     * @return
+     */
+    private Node minimum(Node node){
+        if (node.left == null)
+            return node;
+        return minimum(node.left);
+    }
+
+    /**
+     * 删除以node为根的二分搜索树中最小的节点
+     * 返回删除节点后新的二分搜索树的根
+     * @param node
+     * @return
+     */
+    private Node removeMin(Node node) {
+        if(node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+
+    }
+
+    /**
+     * 从二分搜索树中删除元素为e的节点
+     * @param e
+     */
+    public V remove(K key){
+        Node node = getNode(root,key);
+        if (node!= null){
+            root = remove(root,key);
+            return node.value;
+        }
+        return null;
+    }
+
+    /**
+     * 删除以node为根的二分搜索树中值为e的节点，递归算法
+     * 返回删除节点后的新的二分搜索树的根
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node remove(Node node, K key) {
+        if(node == null)
+            return null;
+        if (key.compareTo(node.key)<0){
+            node.left = remove(node.left,key);
+            return node;
+        }else if (key.compareTo(node.key)>0){
+            node.right = remove(node.right,key);
+            return node;
+        }else{
+            //删除只有右子树的节点
+            if (node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            //删除只有左子树的节点
+            if (node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+            //删除左右子树都有的节点
+            //找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left=node.right=null;
+            return successor;
+
+        }
+
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            BSTMap<String, Integer> bstMap = new BSTMap<>();
+            for (String word : words) {
+                if (bstMap.contains(word))
+                    bstMap.set(word,bstMap.get(word)+1);
+                else
+                    bstMap.add(word,1);
+            }
+            System.out.println("Total different words: "+bstMap.getSize());
+            System.out.println("Frequency of pride: "+bstMap.get("pride"));
+            System.out.println("Frequency of prejudice: "+bstMap.get("prejudice"));
+        }
+    }
+
+}
