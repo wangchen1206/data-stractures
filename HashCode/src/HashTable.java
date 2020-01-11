aa4b6c2b573c8cdce86af17fb1ff03aef539851b
chen-wang1 <chen.wang1@hp.com>
2020-01-11 14:13:55 +0800

    complete HashTable

--- HashCode/src/HashTable.java
+++ HashCode/src/HashTable.java
@@ -5,16 +5,21 @@ import java.util.TreeMap;
  * @Date: 2020/1/10 14:55
  */
 public class HashTable<K, V> {
+
+    private final int[] capasity = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157
+            , 98317, 196613, 393241, 768433, 1572869, 3145739, 6291469, 12582917
+            , 25165843, 100663319, 201326611, 402653189, 805306457, 1610612741};
+
     private static final int upperTol = 10;
     private static final int lowerTol = 2;
-    private static final int initCapasity = 7;
+    private int capasityIndex = 0;
 
     private TreeMap<K, V>[] hashtable;
     private int M;
     private int size;
 
-    public HashTable(int M) {
-        this.M = M;
+    public HashTable() {
+        this.M = capasity[capasityIndex];
         this.size = 0;
         this.hashtable = new TreeMap[M];
         for (int i = 0; i < M; i++) {
@@ -22,9 +27,6 @@ public class HashTable<K, V> {
         }
     }
 
-    public HashTable() {
-        this(initCapasity);
-    }
 
     private int hash(K key) {
         return (key.hashCode() & 0x7fffffff) % M;
@@ -41,8 +43,8 @@ public class HashTable<K, V> {
         else {
             map.put(key, value);
             size++;
-            if (size >= upperTol * M)
-                resize(2 * M);
+            if (size >= upperTol * M && capasityIndex + 1 < capasity.length)
+                resize(capasity[capasityIndex + 1]);
         }
     }
 
@@ -52,12 +54,11 @@ public class HashTable<K, V> {
         if (map.containsKey(key)) {
             ret = map.get(key);
             size--;
-            if (size < lowerTol * M && M / 2 >= initCapasity)
-                resize(M / 2);
+            if (size < lowerTol * M && capasityIndex - 1 >= 0)
+                resize(capasity[capasityIndex - 1]);
         }
         return ret;
     }
-
 
 
     public void set(K key, V value) {
@@ -76,7 +77,7 @@ public class HashTable<K, V> {
     }
 
     private void resize(int newM) {
-        TreeMap<K,V>[] newHashTable = new TreeMap[newM];
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
         for (int i = 0; i < newM; i++) {
             newHashTable[i] = new TreeMap<>();
         }
@@ -84,10 +85,10 @@ public class HashTable<K, V> {
         //这里为了重新计算hash值，用新的M值
         this.M = newM;
         for (int i = 0; i < oldM; i++) {
-            TreeMap<K,V> map = this.hashtable[i];
-            for (K key:map.keySet()){
+            TreeMap<K, V> map = this.hashtable[i];
+            for (K key : map.keySet()) {
                 //重新根据hash值放入新的newHashTable
-                newHashTable[hash(key)].put(key,map.get(key));
+                newHashTable[hash(key)].put(key, map.get(key));
             }
         }
         this.hashtable = newHashTable;
